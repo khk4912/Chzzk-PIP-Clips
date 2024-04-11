3e0c4978de100618e56d703a2a4ac06d875231c5
khk4912 <30457148+khk4912@users.noreply.github.com>
2024-04-12 03:58:23 +0900

    💄 feat: Use useNavigate()

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,36 +1,37 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 import Logo from '../assets/logo.png'
 import styles from '../styles/menu.module.css'
 
-const Menu = (): JSX.Element => (
-  <nav className={styles.navbar}>
-    <div className={styles.navLogo}>
-      <img src={Logo} alt='Chzzk-PIP 로고' />
-      <span id={styles.logoText}>Chzzk-PIP</span>
-    </div>
-    <ul className={styles.navItems}>
-      <li>
-        <Link to='/'>
-          <span>홈</span>
-        </Link>
-      </li>
-      <li>
-        <Link to='/'>
-          <span>인기</span>
-        </Link>
-      </li>
-      <li>
-        <Link to='/'>
-          <span>급상승</span>
-        </Link>
-      </li>
-    </ul>
+const Menu = (): JSX.Element => {
+  const navigate = useNavigate()
 
-    <div className={styles.searchBar}>
-      <input type='text' name='' id='' placeholder='검색...' />
-    </div>
-  </nav>
-)
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.navLogo}>
+        <img src={Logo} alt='Chzzk-PIP 로고' />
+        <span id={styles.logoText}>Chzzk-PIP</span>
+      </div>
+      <ul className={styles.navItems}>
+        <li onClick={() => { navigate('/') }}>
+          <span>홈</span>
+        </li>
+        <li onClick={() => {
+          navigate('/trending')
+        }}
+        >
+          <span>인기</span>
+        </li>
+        <li>
+          <span>급상승</span>
+        </li>
+      </ul>
+
+      <div className={styles.searchBar}>
+        <input type='text' name='' id='' placeholder='검색...' />
+      </div>
+    </nav>
+  )
+}
 
 export default Menu
