f18314d95cffe37d0accb8cc64a4c1f375f25e2d
khk4912 <30457148+khk4912@users.noreply.github.com>
2024-04-11 21:38:42 +0900

    🎉

---
+++ src/main.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode />
+)
