4ca6d7051e0272bc1f7684c8f1dc7cfa2fab24b3
khk4912 <30457148+khk4912@users.noreply.github.com>
2024-04-13 20:17:40 +0900

    🩹 fix: Add playsinline & type checking

--- src/components/WatchClip.tsx
+++ src/components/WatchClip.tsx
@@ -3,10 +3,11 @@ import { useParams } from 'react-router-dom'
 
 const WatchClip = (): JSX.Element => {
   const { id } = useParams()
+  if (id === undefined) return <div>클립 ID가 없습니다.</div>
 
   return (
     <div className={style.watch}>
-      <video src={`https://clips.kosame.dev/${id}`} controls autoPlay />
+      <video src={`https://clips.kosame.dev/${id}`} controls autoPlay playsInline />
 
       <div className={style.clipInfo}>
         <h2>클립 정보</h2>
