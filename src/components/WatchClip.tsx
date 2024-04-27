4c35fdd096d4fff8d226a31f09de50bddd0c15ad
khk4912 <30457148+khk4912@users.noreply.github.com>
2024-04-27 10:17:39 +0900

    ✨ feat: Check clip info

--- src/components/WatchClip.tsx
+++ src/components/WatchClip.tsx
@@ -1,19 +1,40 @@
+import useSWR from 'swr'
 import style from '../styles/watch.module.css'
 import { useParams } from 'react-router-dom'
+import { getClip } from '../api/clip'
+
+import Question from '../assets/question.svg'
 
 const WatchClip = (): JSX.Element => {
   const { id } = useParams()
-  if (id === undefined) return <div>클립 ID가 없습니다.</div>
+  // if (id === undefined) return <div>클립 ID가 없습니다.</div>
+
+  const { data, isLoading } = useSWR(`${id ?? ''}`, getClip)
+  const isDataValid = data?.status === 'success'
 
   return (
-    <div className={style.watch}>
-      <video src={`https://r2-clips.kosame.dev/${id}`} controls autoPlay playsInline />
+    <>
+      {isLoading && <div>로딩 중...</div>}
 
-      <div className={style.clipInfo}>
-        <h2>클립 정보</h2>
-        <p>클립 ID: {id}</p>
-      </div>
-    </div>
+      {!isLoading && !isDataValid &&
+        <div className={style.noClips}>
+          <div id={style.msg}>
+            <img src={Question} alt='Question mark' />
+            <span>클립을 찾을 수 없어요.
+            </span>
+          </div>
+        </div>}
+
+      {isDataValid &&
+        <div className={style.watch}>
+          <video src={`https://r2-clips.kosame.dev/${data?.id}`} controls autoPlay playsInline />
+
+          <div className={style.clipInfo}>
+            <h2>클립 정보</h2>
+            <p>클립 ID: {id}</p>
+          </div>
+        </div>}
+    </>
   )
 }
 
